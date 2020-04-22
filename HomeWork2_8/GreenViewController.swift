dcf75decb6fd9bf665205537b19c5e52573d3e83
moremist <rbest15@rambler.ru>
2020-04-22 19:30:18 +0300

    Original swizzle file implemented

--- HomeWork2_8/GreenViewController.swift
+++ HomeWork2_8/GreenViewController.swift
@@ -3,10 +3,9 @@ import UIKit
 class GreenViewController: UIViewController {
 
     @IBOutlet weak var greenLabel: UILabel!
+    var greenText = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
+        greenLabel.text = greenText
     }
-
 }
--- HomeWork2_8/RedViewController.swift
+++ HomeWork2_8/RedViewController.swift
@@ -3,9 +3,10 @@ import UIKit
 class RedViewController: UIViewController {
 
     @IBOutlet weak var redLabel: UILabel!
+    var redText = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        redLabel.text = redText
     }
 
 }
